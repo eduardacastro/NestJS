26fc26a6dc245e1948d5f81bec0fb965627e7a47
Eduarda de Castro Guterres <86486666+eduardacastro@users.noreply.github.com>
2023-01-03 10:21:49 -0300

    car dealership

---
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(
+        private readonly carsService: CarsService
+    ){}
+
+    @Get()
+    getAllCars(){
+        return this.carsService.findAll();
+    }
+
+    @Get(':id')
+    getCarById( @Param('id', new ParseUUIDPipe({ version: '4',})) id: string) {   
+        console.log({id});
+
+        return this.carsService.findOneById(id);
+        
+    }
+
+    @Post()
+    createCar( @Body() body: any){
+        return body;
+    }
+
+    @Patch('/:id')
+    updateCar( 
+        @Param('id') id: string,
+        @Body() body: any)
+    {
+        return body;
+    }
+
+    @Delete()
+    deleteCar( @Param('id') id: string){
+        return{
+            method: 'delete',
+            id
+        }
+    }
+}
+
